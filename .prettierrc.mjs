381d7bce278836f45acab663256ee85e39c92796
Jared Christensen <jared.christensen@dhigroupinc.com>
2025-04-18 17:24:14 -0500

    Update Prettier configuration to increase print width and remove unused options

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -2,13 +2,7 @@
  * @type {import('prettier').Options}
  */
 export default {
-  printWidth: 80,
-  tabWidth: 2,
-  useTabs: false,
-  semi: false,
-  singleQuote: false,
-  trailingComma: "none",
-  bracketSpacing: true,
+  printWidth: 120,
   bracketSameLine: true,
   plugins: ["@ianvs/prettier-plugin-sort-imports"],
   importOrder: [

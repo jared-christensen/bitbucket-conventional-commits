4809ab5847830a96fc49be0cc9e2aa7c39443105
Jared Christensen <jared.christensen@dhigroupinc.com>
2025-05-23 09:55:12 -0500

    refactor(document-observer): remove console logs from MutationObserver callback

--- src/utils/document-observer.ts
+++ src/utils/document-observer.ts
@@ -4,11 +4,9 @@ export function documentObserver(handleModalInitialization: (modal: HTMLElement)
   const selector = ".atlaskit-portal-container";
 
   const observer = new MutationObserver(() => {
-    console.log("[plasmo] MutationObserver triggered");
     const modal = findModal();
     if (modal && !modal.dataset.opened) {
       handleModalInitialization(modal);
-      console.log("[plasmo] bitbucket pull request modal opened");
       modal.dataset.opened = "true";
     }
   });
